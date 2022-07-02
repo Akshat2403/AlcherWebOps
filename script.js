87fc01c95afc05e96431b0108236c0ebd0388bfc
Akshat2403 <95174507+Akshat2403@users.noreply.github.com>
2022-07-02 18:39:34 +0530

    Task3

---
+++ script.js
@@ -0,0 +1,9 @@
+var b1=document.getElementById("b1")
+var b2=document.getElementById("b2")
+var head=document.getElementById("main-h1");
+b1.onclick=function(){
+    head.innerHTML="You've clicked button-1";
+}
+b2.onclick=function(){
+    head.innerHTML="You've clicked button-2";
+}
